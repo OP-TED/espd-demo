39c2dd9b7ed9fba262f61cdc8fe4d60695d17942
dragos-eu <Dragos-Constantin.STOICA@ext.ec.europa.eu>
2024-09-12 17:45:57 +0200

    v4.0.0 select Selection Criteria

--- ESPD/examples/exclusionGrounds.js
+++ ESPD/examples/exclusionGrounds.js
@@ -16,17 +16,15 @@ Vue.component('exclusionGrounds', {
     },
 
     created() {
-        //console.log('exclusion grounds')
         //Build the UI part for each Criteria
-        if (window.espd_doc.role == 'eo') {
-            for (const key in this.meta_espd_doc.partIII) {
-                if (Object.hasOwn(this.meta_espd_doc.partIII, key)) {
-                    for (const el of this.meta_espd_doc.partIII[key]) {
-                        this.espd_structure.partIII[key].push(`${window.espd_doc.espd_version}-${el}`)
-                    }
+        for (const key in this.meta_espd_doc.partIII) {
+            if (Object.hasOwn(this.meta_espd_doc.partIII, key)) {
+                for (const el of this.meta_espd_doc.partIII[key]) {
+                    this.espd_structure.partIII[key].push(`${window.espd_doc.espd_version}-${el}`)
                 }
             }
         }
+
     },
 
     template: `
@@ -47,11 +45,7 @@ Vue.component('exclusionGrounds', {
             <b-button block v-b-toggle.accordion-e1 variant="info">A: Grounds relating to criminal convictions</b-button>
         </b-card-header>
         <b-collapse id="accordion-e1" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIII']['A']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIII']['A']">
+            <b-card-body v-for="item in espd_structure['partIII']['A']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
@@ -62,11 +56,7 @@ Vue.component('exclusionGrounds', {
             <b-button block v-b-toggle.accordion-e2 variant="info">B: Grounds relating to the payment of taxes or social security contributions </b-button>
         </b-card-header>
         <b-collapse id="accordion-e2" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIII']['B']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIII']['B']">
+            <b-card-body v-for="item in espd_structure['partIII']['B']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
@@ -77,11 +67,7 @@ Vue.component('exclusionGrounds', {
             <b-button block v-b-toggle.accordion-e3 variant="info">C: Grounds relating to insolvency, conflicts of interests or professional misconduct</b-button>
         </b-card-header>
         <b-collapse id="accordion-e3" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIII']['C']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIII']['C']">
+            <b-card-body v-for="item in espd_structure['partIII']['C']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
@@ -92,11 +78,7 @@ Vue.component('exclusionGrounds', {
             <b-button block v-b-toggle.accordion-e4 variant="info">D: Purely national exclusion grounds</b-button>
         </b-card-header>
         <b-collapse id="accordion-e4" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIII']['D']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIII']['D']">
+            <b-card-body v-for="item in espd_structure['partIII']['D']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
--- ESPD/examples/selectionCriteria.js
+++ ESPD/examples/selectionCriteria.js
@@ -18,33 +18,23 @@ Vue.component('selectionCriteria', {
     },
 
     computed: {
-        getTag(item){
-            return item.substring(item.indexOf('-')+1)
+        getTag(item) {
+            return item.substring(item.indexOf('-') + 1)
         }
     },
 
     created() {
-        for (let index = 1; index <= window.espd_doc.procedure.number_of_lots; index++) {
-            this.options.push(`LOT-${index.toString().padStart(4,"0")}`)
+        window.espd_doc.options = []
+        for (let index = 0; index < window.espd_doc.procedure.number_of_lots; index++) {
+            window.espd_doc.options.push(`LOT-${index.toString().padStart(4, "0")}`)
+            this.options.push(`LOT-${index.toString().padStart(4, "0")}`)
         }
-        if (window.espd_doc.role == 'ca'){
-            for (const key in this.meta_espd_doc.partIV) {
-                if (Object.hasOwn(this.meta_espd_doc.partIV, key)) {
-                    for (const el of this.meta_espd_doc.partIV[key]) {
-                        this.espd_structure.partIV[key].push(`${window.espd_doc.espd_version}-${el}`)
-                        window.espd_model[el].lots = this.options
-                    }
-                }
-            }
-        }
-        //Build the UI part for each Criteria
-        if (window.espd_doc.role == 'eo') {
-            for (const key in this.meta_espd_doc.partIV) {
-                if (Object.hasOwn(this.meta_espd_doc.partIV, key)) {
-                    for (const el of this.meta_espd_doc.partIV[key]) {
-                        this.espd_structure.partIV[key].push(`${window.espd_doc.espd_version}-${el}`)
-                        window.espd_model[el].lots = this.options
-                    }
+        for (const key in this.meta_espd_doc.partIV) {
+            if (Object.hasOwn(this.meta_espd_doc.partIV, key)) {
+                for (const el of this.meta_espd_doc.partIV[key]) {
+                    this.espd_structure.partIV[key].push(`${window.espd_doc.espd_version}-${el}`)
+                    window.espd_model[el].lots = this.options
+                    window.espd_model[el].selected = 'OK'
                 }
             }
         }
@@ -68,51 +58,7 @@ Vue.component('selectionCriteria', {
             <b-button block v-b-toggle.accordion-s1 variant="info">A: Suitability</b-button>
         </b-card-header>
         <b-collapse id="accordion-s1" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIV']['A']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-                <!-- LOT management -->
-                <b-form-group label="LOT Management" label-for="tags-component-select_item">
-                <b-form-tags id="tags-component-select_item" v-model="espd_model[item].lots"
-                    size="lg" class="mb-2" add-on-change no-outer-focus>
-                    <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
-                    <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-                        <li v-for="tag in tags" :key="tag" class="list-inline-item">
-                        <b-form-tag @remove="removeTag(tag)" :title="tag" :disabled="disabled" variant="info">{{ tag }}</b-form-tag>
-                        </li>
-                    </ul>
-                    <b-form-select v-bind="inputAttrs" v-on="inputHandlers" :disabled="disabled || options.length === 0" :options="options">
-                        <template #first>
-                        <!-- This is required to prevent bugs with Safari -->
-                        <option disabled value="">Choose a LOT...</option>
-                        </template>
-                    </b-form-select>
-                    </template>
-                </b-form-tags>
-                </b-form-group>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIV']['A']">
-                
-                <!-- LOT management -->
-                <b-form-group label="LOT Management" label-for="tags-component-select_item">
-                <b-form-tags id="tags-component-select_item" v-model="espd_model[item.substring(item.indexOf('-')+1)].lots"
-                    size="lg" class="mb-2" add-on-change no-outer-focus>
-                    <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
-                    <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-                        <li v-for="tag in tags" :key="tag" class="list-inline-item">
-                        <b-form-tag @remove="removeTag(tag)" :title="tag" :disabled="disabled" variant="info">{{ tag }}</b-form-tag>
-                        </li>
-                    </ul>
-                    <b-form-select v-bind="inputAttrs" v-on="inputHandlers" :disabled="disabled || options.length === 0" :options="options">
-                        <template #first>
-                        <!-- This is required to prevent bugs with Safari -->
-                        <option disabled value="">Choose a LOT...</option>
-                        </template>
-                    </b-form-select>
-                    </template>
-                </b-form-tags>
-                </b-form-group>
-                
+            <b-card-body v-for="item in espd_structure['partIV']['A']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
@@ -123,30 +69,7 @@ Vue.component('selectionCriteria', {
             <b-button block v-b-toggle.accordion-s2 variant="info">B: Economic and financial standing</b-button>
         </b-card-header>
         <b-collapse id="accordion-s2" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIV']['B']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-                <!-- LOT management -->
-                <b-form-group label="LOT Management" label-for="tags-component-select_item">
-                <b-form-tags id="tags-component-select_item" v-model="espd_model[item].lots"
-                    size="lg" class="mb-2" add-on-change no-outer-focus>
-                    <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
-                    <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-                        <li v-for="tag in tags" :key="tag" class="list-inline-item">
-                        <b-form-tag @remove="removeTag(tag)" :title="tag" :disabled="disabled" variant="info">{{ tag }}</b-form-tag>
-                        </li>
-                    </ul>
-                    <b-form-select v-bind="inputAttrs" v-on="inputHandlers" :disabled="disabled || options.length === 0" :options="options">
-                        <template #first>
-                        <!-- This is required to prevent bugs with Safari -->
-                        <option disabled value="">Choose a LOT...</option>
-                        </template>
-                    </b-form-select>
-                    </template>
-                </b-form-tags>
-                </b-form-group>                
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIV']['B']">
+            <b-card-body v-for="item in espd_structure['partIV']['B']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
@@ -157,30 +80,7 @@ Vue.component('selectionCriteria', {
             <b-button block v-b-toggle.accordion-s3 variant="info">C: Technical and professional ability</b-button>
         </b-card-header>
         <b-collapse id="accordion-s3" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIV']['C']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-                <!-- LOT management -->
-                <b-form-group label="LOT Management" label-for="tags-component-select_item">
-                <b-form-tags id="tags-component-select_item" v-model="espd_model[item].lots"
-                    size="lg" class="mb-2" add-on-change no-outer-focus>
-                    <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
-                    <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-                        <li v-for="tag in tags" :key="tag" class="list-inline-item">
-                        <b-form-tag @remove="removeTag(tag)" :title="tag" :disabled="disabled" variant="info">{{ tag }}</b-form-tag>
-                        </li>
-                    </ul>
-                    <b-form-select v-bind="inputAttrs" v-on="inputHandlers" :disabled="disabled || options.length === 0" :options="options">
-                        <template #first>
-                        <!-- This is required to prevent bugs with Safari -->
-                        <option disabled value="">Choose a LOT...</option>
-                        </template>
-                    </b-form-select>
-                    </template>
-                </b-form-tags>
-                </b-form-group>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIV']['C']">
+            <b-card-body v-for="item in espd_structure['partIV']['C']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
@@ -191,30 +91,7 @@ Vue.component('selectionCriteria', {
             <b-button block v-b-toggle.accordion-s4 variant="info">D: Quality assurance schemes and environmental management standards</b-button>
         </b-card-header>
         <b-collapse id="accordion-s4" accordion="my-accordion" role="tabpanel">
-            <b-card-body v-if="window.espd_doc.role=='ca'" v-for="item in meta_espd_doc['partIV']['D']">
-                <strong>{{ espd_model[item].name }}</strong>
-                <p>{{ espd_model[item].description }}</p>
-                <!-- LOT management -->
-                <b-form-group label="LOT Management" label-for="tags-component-select_item">
-                <b-form-tags id="tags-component-select_item" v-model="espd_model[item].lots"
-                    size="lg" class="mb-2" add-on-change no-outer-focus>
-                    <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
-                    <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-                        <li v-for="tag in tags" :key="tag" class="list-inline-item">
-                        <b-form-tag @remove="removeTag(tag)" :title="tag" :disabled="disabled" variant="info">{{ tag }}</b-form-tag>
-                        </li>
-                    </ul>
-                    <b-form-select v-bind="inputAttrs" v-on="inputHandlers" :disabled="disabled || options.length === 0" :options="options">
-                        <template #first>
-                        <!-- This is required to prevent bugs with Safari -->
-                        <option disabled value="">Choose a LOT...</option>
-                        </template>
-                    </b-form-select>
-                    </template>
-                </b-form-tags>
-                </b-form-group>
-            </b-card-body>
-            <b-card-body v-if="window.espd_doc.role=='eo'" v-for="item in espd_structure['partIV']['D']">
+            <b-card-body v-for="item in espd_structure['partIV']['D']">
                 <component v-bind:is="item"></component>
             </b-card-body>
         </b-collapse>
